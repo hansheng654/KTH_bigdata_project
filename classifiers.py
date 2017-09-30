5bb54a9bc08e4271fa86c0235c086ba4124c1b77
Johnny Han <johnnyhan654@foxmail.com>
2017-09-30 12:25:30 +0200

    added classifier V1.0, with bayes classifiers ready and linear SVM

---
+++ classifiers.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 30 11:35:29 2017
+
+@author: Johnny
+"""
+
+from data_input import get_sparse_data
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import GridSearchCV
+import numpy as np
+
+[y_train,X_raw_train,X_train_sparse],[y_val,X_raw_val,X_val_sparse],[y_test,X_raw_test,X_test_sparse] = get_sparse_data()
+
+def check_acc(trained_clf,clf_name,val_acc = False):
+    #print training and testing acc automatically 
+    #training acc
+    result_sk = trained_clf.predict(X_train_sparse)
+    
+    true_label =np.array(list(map(int,y_train)))
+    result_sk = np.array(list(map(int,result_sk)))
+    acc = np.mean(result_sk == true_label) *100.
+    print("Training Accuracy, %s with tf-idf: %.2f%%" % (clf_name,acc))
+    
+    #val acc
+    if val_acc:
+        result_sk = trained_clf.predict(X_val_sparse)
+    
+        true_label =np.array(list(map(int,y_val)))
+        result_sk = np.array(list(map(int,result_sk)))
+        acc = np.mean(result_sk == true_label) *100.
+        print("Validation Accuracy, %s with tf-idf: %.2f%%" % (clf_name,acc))
+    
+    
+    #testing acc
+    result_sk = text_clf.predict(X_test_sparse)
+    
+    
+    true_label =np.array(list(map(int,y_test)))
+    result_sk = np.array(list(map(int,result_sk)))
+    acc = np.mean(result_sk == true_label) *100.
+    print("Testing Accuracy, %s with tf-idf: %.2f%%" % (clf_name,acc))
+
+text_clf = BernoulliNB()
+text_clf.fit(X_train_sparse,y_train)
+check_acc(text_clf,"BernoulliNB")
+
+text_clf = MultinomialNB()
+text_clf.fit(X_train_sparse,y_train)
+check_acc(text_clf,"MultinomialNB")
+
+text_clf = SGDClassifier(loss ='hinge',
+              penalty = 'l1',
+              alpha = 0.0002,
+              n_iter = 100,
+              shuffle = True)
+text_clf.fit(X_train_sparse,y_train)
+check_acc(text_clf,"MultinomialNB",True)
+
+parameters = {
+               'alpha': (1e-4,2e-4,3e-4,9e-3)
+               }
+gs_clf = GridSearchCV(text_clf,parameters,n_jobs=2)
+gs_clf = gs_clf.fit(X_val_sparse,y_val)
+gs_clf.best_score_
+gs_clf.best_params_
