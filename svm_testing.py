591ae94b08815bbbdce92b909900c56f3c22471e
jeesupkim <31935469+jeesupkim@users.noreply.github.com>
2017-09-30 21:37:15 +0200

    Update svm_testing.py

--- svm_testing.py
+++ svm_testing.py
@@ -13,9 +13,15 @@ import classifiers
 KERNEL='linear'
 C_PARAM=15
 
+start_time = time.time() 
 [y_train,X_raw_train,X_train_sparse],[y_val,X_raw_val,X_val_sparse],[y_test,X_raw_test,X_test_sparse] = get_sparse_data()
 
-linsvc = svm.SVC(C=C_PARAM, kernel='linear', decision_function_shape='ovo')
+"""
+Tests for three different multi-class SVC, SVC with linear kernel, LinearSVC, and nuSVC with rbf kernel.
+Highest accuracy is SVC with linear kernel with 68.73% on training, 68.89% on validation, 68.09% on Testing.
+Accuracies hold around 67% at the moment
+"""
+linsvc = svm.SVC(C=C_PARAM, kernel=KERNEL, decision_function_shape='ovo')
 LinearSVC = svm.LinearSVC(C=C_PARAM)
 rbf_svc = svm.NuSVC(decision_function_shape='ovo')
 
@@ -27,5 +33,4 @@ classifiers.check_acc(linsvc, "SVC with linear kernel", True)
 classifiers.check_acc(LinearSVC, "LinearSVC", True)
 classifiers.check_acc(rbf_svc, "nuSVC", True)
 
-start_time = time.time() 
 print(" --- extracting time: %s seconds ---" % (time.time() - start_time))
